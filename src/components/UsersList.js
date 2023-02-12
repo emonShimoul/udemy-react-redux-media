af5960a3e06f767a7abfdb397010d2fa09c5cfb3
emonShimoul <emonshimoul@gmail.com>
2023-02-13 00:51:53 +0600

    displaying the fetching data

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 const UsersList = () => {
     const dispatch = useDispatch();
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
 
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error fetching data...</div>
+    }
+
     return (
         <div>
-            User List
+            {data.length}
         </div>
     );
 };
