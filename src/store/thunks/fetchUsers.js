c3e875a082e9e4e1dd919aaff19341d78d30614d
emonShimoul <emonshimoul@gmail.com>
2023-02-13 01:37:38 +0600

    some code for developers testing purpose

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,8 +4,18 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
 
+    // DEV ONLY!!!
+    await pause(5000);
+
     return response.data;
 });
+
+// DEV ONLY!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+}
 
 // fetchUsers.pending === 'users/fetch/pending'
 // fetchUsers.fulfilled === 'users/fetch/fulfilled'
