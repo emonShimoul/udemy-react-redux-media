6e305802a3c78e10ff609effd4ca51c91274c81c
emonShimoul <emonshimoul@gmail.com>
2023-02-13 14:50:30 +0600

    slight changes

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useFetchAlbumsQuery } from '../store';
 
 const AlbumsList = ({ user }) => {
-    const { data, error, isLoading } = useFetchAlbumsQuery(user);
-    console.log(data, error, isLoading);
+    // console.log(user);
+    const results = useFetchAlbumsQuery(user);
+    console.log(results);
+    // console.log({ data, error, isLoading });
 
     return (
         <div>
