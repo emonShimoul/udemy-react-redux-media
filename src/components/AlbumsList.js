ab5c912dc9a2a9207524181d0d15076ef8342428
emonShimoul <emonshimoul@gmail.com>
2023-02-13 15:12:55 +0600

    added another expandable panel

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 const AlbumsList = ({ user }) => {
     // console.log(user);
-    const results = useFetchAlbumsQuery(user);
-    console.log(results);
-    // console.log({ data, error, isLoading });
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else if (error) {
+        content = <div>Error loading a album...</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of Photos in the album
+                </ExpandablePanel>
+            );
+        });
+    }
 
     return (
         <div>
-            Albums for {user.name}
+            <div>Albums for {user.name}</div>
+            <div>{content}</div>
         </div>
     );
 };
