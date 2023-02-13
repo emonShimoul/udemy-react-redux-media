e1ba6f6529a2a48b0da980dfdb50c03a6d3ba6fe
emonShimoul <emonshimoul@gmail.com>
2023-02-13 11:47:14 +0600

    some css im userlist component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -21,27 +21,27 @@ const UsersList = () => {
         doCreateUser();
     };
 
+    let content;
+
     if (isLoadingUsers) {
-        return <Skeleton className="h-10 w-full" times={6} />;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error fetching data...</div>
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className='flex p-2 justify-between items-center cursor-pointer'>
-                    {user.name}
+        content = <Skeleton className="h-10 w-full" times={6} />;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className='flex p-2 justify-between items-center cursor-pointer'>
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        );
-    });
+            );
+        });
+    }
 
     return (
         <div>
-            <div className='flex flex-row justify-between m-3'>
+            <div className='flex flex-row justify-between items-center m-3'>
                 <h1 className='m-2 text-xl'>Users</h1>
                 <Button loading={isCreatingUser} onClick={handleUserAdd}>
                     +Add User
@@ -49,7 +49,7 @@ const UsersList = () => {
 
                 {creatingUserError && 'error creatng user...'}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 };
