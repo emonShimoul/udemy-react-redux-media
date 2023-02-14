95d43cfaa7777323ff7e1d6da602a4a868f1099b
emonShimoul <emonshimoul@gmail.com>
2023-02-14 21:04:35 +0600

    adding the endpoints

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useFetchPhotosQuery } from '../store';
 
 const PhotosList = ({ album }) => {
+    useFetchPhotosQuery(album);
+
     return (
         <div>
             <h3>Photos List</h3>
